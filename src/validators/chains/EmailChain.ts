1622632f4217fa89bb09d19123f4f510a3bde0e1
Gustavo Jesinski <gustavojesinskib@gmail.com>
2025-03-22 11:44:29 -0300

    fix branch

--- src/validators/chains/EmailChain.ts
+++ src/validators/chains/EmailChain.ts
@@ -14,7 +14,7 @@ export class EmailChain extends ChainableValidator {
 
     this.setNext(emailIsRequiredValidation)
       .setNext(emailLengthValidation)
-      .setNext(new EmailMaxLengthValidation())
+      .setNext(EmailMaxLengthValidation)
       .endChain(emailFormatComposite);
   }
   protected validateInternal(payload: any): string[] {
