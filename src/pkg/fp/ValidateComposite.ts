be6b5bca9a4b1597b1774dc14f07af8bec92b6df
Gustavo Jesinski <gustavojesinskib@gmail.com>
2025-06-21 16:19:02 -0300

    flex js knowledge

--- src/pkg/fp/ValidateComposite.ts
+++ src/pkg/fp/ValidateComposite.ts
@@ -4,16 +4,10 @@ export const ValidateComposite = (
   ...args: Array<(payload: any) => Promise<ValidationResult> | ValidationResult>
 ): ((payload: any) => Promise<ValidationResult> | ValidationResult) => {
   return async (payload: any): Promise<ValidationResult> => {
-    const messages: string[] = [];
-    let valid = true;
-
-    for (const validator of args) {
-      // todo: implement like CompositeValidator.ts to flex some JS knowledge
-      const result = await validator(payload);
-      messages.push(...result.messages);
-      valid = valid && result.valid;
-    }
-
-    return { valid, messages };
+    const result = await Promise.all(args.map((fn) => fn(payload)));
+    return {
+      valid: result.every((res) => res.valid),
+      messages: result.flatMap((res) => res.messages),
+    };
   };
 };
